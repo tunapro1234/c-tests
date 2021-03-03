2605d4b4ddca9d5afafa9c836b6cae4b49ec5b59
TUNAPRO1234 <60389660+tunapro1234@users.noreply.github.com>
2021-03-03 12:20:17 +0300

    renamed bin-search

--- search.c
+++ search.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+u32 search_linear(s32 target, s32 *array, u32 array_start_index, u32 array_length){
+	for (int i = array_start_index; i < array_length; i++)
+		if (array[i] == target) { return i; }
+	return -1;
+}
+
 u32 search_binary(s32 target, s32 *array, u32 array_start_index, u32 array_length){
 	u32 mid_point = array_length-1;
 	
@@ -19,7 +25,8 @@ int main(){
 	s32 a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	s32 input; printf(">>> "); scanf("%d", &input);
 	
-	printf("%d", search_binary(input, a, 0, 10));
+	printf("%d\n", search_binary(input, a, 0, 10));
+	printf("%d\n", search_linear(input, a, 0, 10));
 	return 0;
 }
 
